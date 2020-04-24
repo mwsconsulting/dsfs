70b0bb0c40ec2236bc85f15b3e90e5131a769a97
westen30 <westen30@gmail.com>
2020-04-24 13:25:27 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code created while following through the exercises in the book Data Science from Scratch',
+    author='Martin Smith',
+    license='',
+)
